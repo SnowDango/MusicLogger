0709e051f830c9e25e891d9f36ddf561d9bdb3c0
YukiOchiai <xdi.grand.you.winner@gmail.com>
2022-02-19 23:14:21 +0900

    urlとlocal保存がない場合のapiの取得の追加

--- app/src/main/java/com/snowdango/musiclogger/view/model/ArtworkView.kt
+++ app/src/main/java/com/snowdango/musiclogger/view/model/ArtworkView.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.findViewTreeLifecycleOwner
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import com.bumptech.glide.load.DataSource
@@ -30,18 +31,53 @@ class ArtworkView @JvmOverloads constructor(context: Context, attrs: AttributeSe
     private val artwork: ImageView
     private val progress: ProgressBar
     private val requestManager: RequestManager
+    private val helper: ArtworkViewHelper
 
     init {
         LayoutInflater.from(context).inflate(R.layout.artwork_view, this, true)
         artwork = findViewById(R.id.artworkImageView)
         progress = findViewById(R.id.artworkProgress)
         requestManager = Glide.with(context)
+        helper = ArtworkViewHelper()
     }
 
     @SuppressLint("CheckResult")
     fun update(musicMetaWithArt: MusicMetaWithArt, cropType: ImageCrop) {
         start()
         val artworkPath = getArtworkPath(musicMetaWithArt.url, musicMetaWithArt.artworkId, true)
+        if (artworkPath.isBlank()) {
+            fetchApiArtwork(musicMetaWithArt, cropType)
+        } else {
+            loadArtwork(artworkPath, cropType)
+        }
+    }
+
+    fun clear() {
+        requestManager.clear(artwork)
+    }
+
+    private fun start() {
+        artwork.visibility = View.INVISIBLE
+        progress.visibility = View.VISIBLE
+    }
+
+    private fun finish() {
+        artwork.visibility = View.VISIBLE
+        progress.visibility = View.INVISIBLE
+    }
+
+    private fun fetchApiArtwork(musicMetaWithArt: MusicMetaWithArt, cropType: ImageCrop) {
+        findViewTreeLifecycleOwner()?.let { lifecycleOwner ->
+            helper.artworkPath.observe(lifecycleOwner) {
+                it?.let { fetchArtworkPath ->
+                    loadArtwork(fetchArtworkPath, cropType)
+                }
+            }
+            helper.fetchArtwork(musicMetaWithArt)
+        }
+    }
+
+    private fun loadArtwork(artworkPath: String, cropType: ImageCrop) {
         requestManager.customRequestBuilder(cropType).listener(object : RequestListener<Drawable> {
             override fun onLoadFailed(
                 e: GlideException?,
@@ -67,20 +103,6 @@ class ArtworkView @JvmOverloads constructor(context: Context, attrs: AttributeSe
         }).load(artworkPath).into(artwork)
     }
 
-    fun clear() {
-        requestManager.clear(artwork)
-    }
-
-    private fun start() {
-        artwork.visibility = View.INVISIBLE
-        progress.visibility = View.VISIBLE
-    }
-
-    private fun finish() {
-        artwork.visibility = View.VISIBLE
-        progress.visibility = View.INVISIBLE
-    }
-
     private fun getArtworkPath(url: String?, artworkId: String?, isDetail: Boolean): String {
         url?.let {
             return if (isDetail) {
@@ -100,5 +122,4 @@ class ArtworkView @JvmOverloads constructor(context: Context, attrs: AttributeSe
         }
         return ""
     }
-
 }
---
+++ app/src/main/java/com/snowdango/musiclogger/view/model/ArtworkViewHelper.kt
@@ -0,0 +1,43 @@
+package com.snowdango.musiclogger.view.model
+
+import androidx.lifecycle.MutableLiveData
+import com.snowdango.musiclogger.DETAIL_IMAGE_SIZE
+import com.snowdango.musiclogger.repository.api.ApiProvider
+import com.snowdango.musiclogger.repository.db.dao.entity.MusicMetaWithArt
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.io.path.Path
+
+class ArtworkViewHelper {
+
+    val artworkPath: MutableLiveData<String?> = MutableLiveData<String?>().also {
+        it.value = null
+    }
+
+    fun fetchArtwork(musicMetaWithArt: MusicMetaWithArt) {
+        musicMetaWithArt.mediaId?.let { getSongData(it) }
+    }
+
+    private fun getSongData(mediaId: String) = CoroutineScope(Dispatchers.Default).launch {
+        val apiResult = try {
+            ApiProvider.appleApi.getSongInfo(mediaId).execute()
+        } catch (e: Exception) {
+            null
+        }
+        apiResult?.let {
+            it.body()?.appleSearchResults?.let { list ->
+                if (list.isNotEmpty()) {
+                    list[0].artworkUrl100?.let { artworkUrl100 ->
+                        artworkPath.postValue(generateArtworkUrl(artworkUrl100))
+                    }
+                }
+            }
+        }
+    }
+
+    private fun generateArtworkUrl(baseUrl: String): String {
+        val imagePath = Path(baseUrl)
+        return baseUrl.replace(imagePath.fileName.toString(), "${DETAIL_IMAGE_SIZE}x$DETAIL_IMAGE_SIZE.jpg")
+    }
+}
